1fa92394298c1296538f1b1adee679742913cbd3
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-04-09 21:36:59 +0530

    Update script.js

--- Hacker/script.js
+++ Hacker/script.js
@@ -1,1 +1,39 @@
+let hack = [
+    `Initializing hack program.`,
+    `Program initialized. Connecting to the device.`,
+    `Device connected.`,
+    `Device username: XXXXXXXXXX`,
+    `Unlocking the device.`,
+    `100 passwords tried.`,
+    `Another 100 passwords tried.`,
+    `Device unlocked.`,
+    `Connecting to the server.`,
+    `Server connected.`,
+    `Copying device data to the server.`,
+    `Copy in progress.`,
+    `Copy completed.`,
+    `Hack successful.`
+]
+
+const sleep = async (seconds) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(true)
+        }, seconds * 2000)
+    })
+}
+
+const showHack = async (message) => {
+    await sleep(2)
+    hack_texts.innerHTML = hack_texts.innerHTML + message + "<br>"
+}
+
+(async () => {
+    for (let i = 0; i < hack.length; i++) {
+        await showHack(hack[i])
+    }
+})()
+
+
+
 
