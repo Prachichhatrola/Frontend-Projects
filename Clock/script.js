7a7df1d416f802e1e4b7d9d580da8665d1ee2d51
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-04-12 08:37:54 +0530

    Update script.js

--- Clock/script.js
+++ Clock/script.js
@@ -1,1 +1,23 @@
+function displayTime(){
+  let dateTime = new Date();
+  let hrs = dateTime.getHours();
+  let min = dateTime.getMinutes();
+  let sec = dateTime.getSeconds();
+  let session = document.getElementById('session')
+  if(hrs>=12){
+    session.innerHTML = 'PM';
+  }else{
+    session.innerHTML = 'AM'
+  }
+
+  
+  document.getElementById('hours').innerHTML = hrs;
+  document.getElementById('minutes').innerHTML = min;
+  document.getElementById('seconds').innerHTML = sec;
+  
+}
+
+setInterval(displayTime,10)
+
+
 
